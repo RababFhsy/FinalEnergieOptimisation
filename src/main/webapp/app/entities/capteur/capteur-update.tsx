53b7b25d063cbee8a1094e223d467cae3051bf54
Helk20 <houdaelkoraini@gmail.com>
2023-10-20 23:56:04 +0100

    add sonsor resolved

--- src/main/webapp/app/entities/capteur/capteur-update.tsx
+++ src/main/webapp/app/entities/capteur/capteur-update.tsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Button, Row, Col, FormText } from 'reactstrap';
+import { isNumber, ValidatedField, ValidatedForm, ValidatedBlobField } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { convertDateTimeFromServer, convertDateTimeToServer, displayDefaultDateTime } from 'app/shared/util/date-utils';
+import { mapIdList } from 'app/shared/util/entity-utils';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
+
+import { ICapteur } from 'app/shared/model/capteur.model';
 import { getEntity, updateEntity, createEntity, reset } from './capteur.reducer';
 
 export const CapteurUpdate = () => {
   const dispatch = useAppDispatch();
+
   const navigate = useNavigate();
+
   const { id } = useParams<'id'>();
   const isNew = id === undefined;
+
   const capteurEntity = useAppSelector(state => state.capteur.entity);
   const loading = useAppSelector(state => state.capteur.loading);
   const updating = useAppSelector(state => state.capteur.updating);
@@ -51,54 +59,50 @@ export const CapteurUpdate = () => {
     isNew
       ? {}
       : {
-        ...capteurEntity,
-      };
+          ...capteurEntity,
+        };
 
   return (
-    <div className="custom-container" style={{ maxWidth: '1000px', margin: '0 auto', padding: '50px' }}>
-      <h2 style={{ marginBottom: '20px' }}>Create Sensor</h2>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <Form>
-            {!isNew ? (
-              <FormGroup>
-                <Label for="capteur-id">ID</Label>
-                <Input type="text" name="id" id="capteur-id" readOnly />
-              </FormGroup>
-            ) : null}
-            <FormGroup>
-              <Label for="capteur-capteurReference">Sensor Reference</Label>
-              <Input type="text" name="capteurReference" id="capteur-capteurReference" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="capteur-type">Type</Label>
-              <Input type="text" name="type" id="capteur-type" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="capteur-photo">Sensor Image</Label>
-              <Input type="file" name="photo" id="capteur-photo" accept="image/*" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="capteur-valeurMin">Min Value</Label>
-              <Input type="text" name="valeurMin" id="capteur-valeurMin" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="capteur-valeurMax">Max Value</Label>
-              <Input type="text" name="valeurMax" id="capteur-valeurMax" />
-            </FormGroup>
-            <Button tag={Link} to="/capteur" replace color="info">
-              <FontAwesomeIcon icon="arrow-left" />
+    <div>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <h2 id="feOptimisationEnergieApp.capteur.home.createOrEditLabel" data-cy="CapteurCreateUpdateHeading">
+            Create or edit a Capteur
+          </h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <ValidatedForm defaultValues={defaultValues()} onSubmit={saveEntity}>
+              {!isNew ? <ValidatedField name="id" required readOnly id="capteur-id" label="ID" validate={{ required: true }} /> : null}
+              <ValidatedField
+                label="Capteur Reference"
+                id="capteur-capteurReference"
+                name="capteurReference"
+                data-cy="capteurReference"
+                type="text"
+              />
+              <ValidatedField label="Type" id="capteur-type" name="type" data-cy="type" type="text" />
+              <ValidatedBlobField label="Photo" id="capteur-photo" name="photo" data-cy="photo" isImage accept="image/*" />
+              <ValidatedField label="Valeur Min" id="capteur-valeurMin" name="valeurMin" data-cy="valeurMin" type="text" />
+              <ValidatedField label="Valeur Max" id="capteur-valeurMax" name="valeurMax" data-cy="valeurMax" type="text" />
+              <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/capteur" replace color="info">
+                <FontAwesomeIcon icon="arrow-left" />
+                &nbsp;
+                <span className="d-none d-md-inline">Back</span>
+              </Button>
               &nbsp;
-              <span>Back</span>
-            </Button>
-            &nbsp;
-            <Button color="primary" type="submit" disabled={updating}>
-              <FontAwesomeIcon icon="save" />
-              &nbsp; Save
-            </Button>
-          </Form>
-        </div>
-      </div>
+              <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
+                <FontAwesomeIcon icon="save" />
+                &nbsp; Save
+              </Button>
+            </ValidatedForm>
+          )}
+        </Col>
+      </Row>
     </div>
   );
 };
